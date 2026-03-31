98c037dbb795902c83444c4ea1ca8e7463e3bbbb
Tarakaram9 <tarakaram9@github.com>
2026-03-31 21:43:34 +0100

    fix: remove Titlebar and replace h-screen with h-full to fix top section clipping on Windows

--- studio/src/components/Layout/Layout.tsx
+++ studio/src/components/Layout/Layout.tsx
@@ -1,11 +1,9 @@
 import { ReactNode } from 'react'
 import Sidebar from './Sidebar'
-import Titlebar from './Titlebar'
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <div className="flex flex-col h-screen bg-surface-900 overflow-hidden">
-      <Titlebar />
+    <div className="flex flex-col h-full bg-surface-900 overflow-hidden">
       <div className="flex flex-1 overflow-hidden">
         <Sidebar />
         <main className="flex-1 overflow-hidden bg-surface-800">
